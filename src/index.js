cc11685c9dede5ed704856cf6815f2e6f98c55f4
Priyadj <priyadjain1996@gmail.com>
2021-11-03 00:28:59 +0530

    Add

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 var http = require("http");
+const fs = require("fs");
 
 const httpServer = http.createServer(handleServer);
 
 
 function handleServer(req, res) {
+    if(req.url == '/welcome'){
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write('Welcome to Dominos!');
+        res.end();
+        return;
+    }
+    if(req.url == '/contact'){
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.write( JSON.stringify({
+            'phone': '18602100000',
+            'email': 'guestcaredominos@jublfood.com'
+        }));
+        res.end();
+        return;
+    }
   
 }
+httpServer.listen(3000);
 
-module.exports = httpServer;
+
+
