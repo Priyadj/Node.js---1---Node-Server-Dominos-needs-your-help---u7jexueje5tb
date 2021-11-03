e8e8ab6305b0f50444d9fafa9dc63fa1d78ec43e
Priyadj <priyadjain1996@gmail.com>
2021-11-03 21:59:38 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,36 +1,32 @@
 const http = require("http");
 const fs = require("fs");
-const express = require("express");
-
-const app = express() 
-const welcome = express() 
-const contact =express()
-
-welcome.get('/', function (req, res) {
-    res.status(200);
-    res.setHeader('Content-Type', 'text/plain')
-    res.send('Welcome to Dominos!')
-
-})
-
-contact.get('/', function (req, res) {
-    res.status(200);
-    res.setHeader('Content-Type', 'application/json')
-    res.send({
-        'phone': '18602100000',
-        'email': 'guestcaredominos@jublfood.com'
-      })
-  
- 
-    })
-
-app.use('/welcome', welcome);
-app.use('/contact', contact);
-
-const httpServer = http.createServer(app);
 
 
-httpServer.listen(3000);
+
+
+const httpServer = http.createServer(handleServer);
+function handleServer(req,res) {
+    if(req.url === "/welcome"){
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.write('Welcome to Dominos!');
+        res.end();
+        return;
+    }
+    if(req.url === "/contact"){
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.write(JSON.stringify({
+            'phone': '18602100000',
+            'email': 'guestcaredominos@jublfood.com'
+          }));
+        res.end();
+        return;
+    }
+}
+
+
+httpServer.listen(8081);
 
 
 
